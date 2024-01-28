0bb22e062834a1e4cbeb44652b455dd4e1cd6668
Dan Nicholls <dannicholls12@gmail.com>
2024-01-28 20:05:12 +1100

    Submitted #576 and refactored using memoization

--- 576.out-of-boundary-paths-python3.py
+++ 576.out-of-boundary-paths-python3.py
@@ -3,28 +3,38 @@ class Solution:
     def findPaths(
         self, m: int, n: int, maxMove: int, startRow: int, startColumn: int
     ) -> int:
-        if maxMove == 0:
-            return 0
+        MODULO = 10**9 + 7
+        moveCache = {}
 
-        outOfBoundsPaths = 0
+        def calcMoves(currentRow: int, currentCol: int, remainingMoves: int) -> int:
+            if (currentRow, currentCol, remainingMoves) in moveCache:
+                return moveCache[(currentRow, currentCol, remainingMoves)]
 
-        availableMoves = [
-            (startRow - 1, startColumn),
-            (startRow + 1, startColumn),
-            (startRow, startColumn - 1),
-            (startRow, startColumn + 1),
-        ]
+            if currentRow < 0 or currentRow >= m or currentCol < 0 or currentCol >= n:
+                return 1
 
-        print(f"availableMoves: {availableMoves}")
+            if remainingMoves == 0:
+                return 0
 
-        for moveRow, moveCol in availableMoves:
-            print(f"({startRow}, {startColumn}) -> ({moveRow}, {moveCol})")
-            if moveRow < 0 or moveRow > m or moveCol < 0 or moveCol > n:
-                outOfBoundsPaths += 1
-            else:
-                outOfBoundsPaths += self.findPaths(m, n, maxMove - 1, moveRow, moveCol)
+            outOfBoundsPaths = 0
 
-        return outOfBoundsPaths
+            availableMoves = [
+                (currentRow - 1, currentCol),
+                (currentRow + 1, currentCol),
+                (currentRow, currentCol - 1),
+                (currentRow, currentCol + 1),
+            ]
+
+            print(f"availableMoves: {availableMoves}, remainingMoves: {remainingMoves}")
+
+            for moveRow, moveCol in availableMoves:
+                outOfBoundsPaths += calcMoves(moveRow, moveCol, remainingMoves - 1)
+                outOfBoundsPaths %= MODULO
+
+            moveCache[(currentRow, currentCol, remainingMoves)] = outOfBoundsPaths
+            return outOfBoundsPaths
+
+        return calcMoves(startRow, startColumn, maxMove)
 
 
 # @leet end
