51315210ef62a324bf5c7ee3e3f36e50c4f15b4b
Dan Nicholls <dannicholls12@gmail.com>
2024-02-01 11:47:55 +1100

    Submitted solution for #739

--- 739.daily-temperatures-python3.py
+++ 739.daily-temperatures-python3.py
@@ -1,6 +1,26 @@
 # @leet start
 class Solution:
+    # Attempted 2 - Stack & Reverse Iteration
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        if len(temperatures) == 1:
+            return [0]
+
+        result = [0] * len(temperatures)
+
+        stack = []
+
+        for currentDayIndex in range(len(temperatures) - 1, -1, -1):
+            print(currentDayIndex)
+            while stack and temperatures[currentDayIndex] >= temperatures[stack[-1]]:
+                stack.pop()
+            if stack:
+                result[currentDayIndex] = stack[-1] - currentDayIndex
+            stack.append(currentDayIndex)
+        return result
+
+    # Attempted 1 - Loops
+    # Doesn't handle large input lengths well
+    def dailyTemperatures1(self, temperatures: List[int]) -> List[int]:
         if len(temperatures) == 1:
             return [0]
 
@@ -11,7 +31,6 @@ class Solution:
                 if temperatures[currentDayIndex] < temperatures[compareIndex]:
                     result[currentDayIndex] = compareIndex - currentDayIndex
                     break
-
         return result
 
 
