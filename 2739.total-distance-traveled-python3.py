6b79992471d85942e1969f78fe7d83f7f114ebe1
Dan Nicholls <dannicholls12@gmail.com>
2024-02-09 11:41:15 +1100

    Refactored #2739 solution

--- 2739.total-distance-traveled-python3.py
+++ 2739.total-distance-traveled-python3.py
@@ -1,5 +1,17 @@
 # @leet start
 class Solution:
     def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
-        
+        total = 0
+
+        while mainTank >= 5:
+            mainTank -= 5
+            total += 50
+
+            if additionalTank > 0:
+                additionalTank -= 1
+                mainTank += 1
+        total += mainTank * 10
+        return total
+
+
 # @leet end
