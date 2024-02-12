27efe4d3f87ecc64dbf07e8c9f228c646ad02dd7
Dan Nicholls <dannicholls12@gmail.com>
2024-02-12 20:37:59 +1100

    Added improved #169 solution

--- 169.majority-element-python3.py
+++ 169.majority-element-python3.py
@@ -1,5 +1,12 @@
 # @leet start
 class Solution:
+    # Solution 1 - Counter
+    def majorityElement1(self, nums: List[int]) -> int:
+        return Counter(nums).most_common(1)[0][0]
+
+    # Solution 2 - Sorted List
     def majorityElement(self, nums: List[int]) -> int:
-        
+        return nums.sort()[len(nums) // 2]
+
+
 # @leet end
