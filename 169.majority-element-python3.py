f819b873fc1a47355aad90fcd42d5f0f7b8fa968
Dan Nicholls <dannicholls12@gmail.com>
2024-02-12 20:47:58 +1100

    Added updated solution for #169

--- 169.majority-element-python3.py
+++ 169.majority-element-python3.py
@@ -5,8 +5,23 @@ class Solution:
         return Counter(nums).most_common(1)[0][0]
 
     # Solution 2 - Sorted List
+    def majorityElement2(self, nums: List[int]) -> int:
+        return sorted(nums)[len(nums) // 2]
+
+    # Solution 3 - Moores Voting Algorithm
     def majorityElement(self, nums: List[int]) -> int:
-        return nums.sort()[len(nums) // 2]
+        currentCount, currentNum = (0, 0)
+
+        for num in nums:
+            if currentCount == 0:
+                currentNum = num
+
+            if num == currentNum:
+                currentCount += 1
+            else:
+                currentCount -= 1
+
+        return currentNum
 
 
 # @leet end
