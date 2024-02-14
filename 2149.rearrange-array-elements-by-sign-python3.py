0cd1b6b2c990c1eab4f238eda11c88057f7f2315
Dan Nicholls <dannicholls12@gmail.com>
2024-02-14 14:36:16 +1100

    Added solution to #2149

--- 2149.rearrange-array-elements-by-sign-python3.py
+++ 2149.rearrange-array-elements-by-sign-python3.py
@@ -3,6 +3,7 @@ from collections import deque
 
 
 class Solution:
+    # Solution 1 - Deques
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         posVals = deque()
         negVals = deque()
@@ -16,6 +17,22 @@ class Solution:
 
         for pos, neg in zip(posVals, negVals):
             result += [pos, neg]
+        return result
+
+    # Solution 2 - Two Pointers
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        result = [0] * len(nums)
+        posPointer = 0
+        negPointer = 1
+
+        for val in nums:
+            if val > 0:
+                result[posPointer] = val
+                posPointer += 2
+            else:
+                result[negPointer] = val
+                negPointer += 2
+
         return result
 
 
