68df2b1bef0cb1162294ba9668bfcc2edcecedf3
Dan Nicholls <dannicholls12@gmail.com>
2024-02-19 19:12:37 +1100

    Added improved solution for #231

--- 231.power-of-two-python3.py
+++ 231.power-of-two-python3.py
@@ -1,7 +1,12 @@
 # @leet start
 class Solution:
+    # Solution 1 - Binary Val Count
     def isPowerOfTwo(self, n: int) -> bool:
         return n > 0 and bin(n)[2:].count("1") == 1
+
+    # Solution 2 - Binary Shift
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and (n & (n - 1)) == 0
 
 
 # @leet end
