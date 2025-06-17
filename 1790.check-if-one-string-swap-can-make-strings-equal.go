844323501b5e24b85961ccce9f691d0480ac08aa
Dan Nicholls <dannicholls12@gmail.com>
2025-06-17 21:41:27 +1000

    refactor: simplify #1790 solution

--- 1790.check-if-one-string-swap-can-make-strings-equal.go
+++ 1790.check-if-one-string-swap-can-make-strings-equal.go
@@ -1,5 +1,10 @@
 // @leet start
 func areAlmostEqual(s1 string, s2 string) bool {
+	// Check initial matching strings
+	if s1 == s2 {
+		return true
+	}
+
 	incorrectVals := make([]int,0)
 	for i := range s1 {
 		if s1[i] != s2[i] {
@@ -10,9 +15,6 @@ func areAlmostEqual(s1 string, s2 string) bool {
 		}
 	}
 
-	if len(incorrectVals) == 0 || len(incorrectVals) == 2 && (s1[incorrectVals[0]] == s2[incorrectVals[1]]) && (s2[incorrectVals[0]] == s1[incorrectVals[1]]){
-		return true	
-	}
-	return false
+	return len(incorrectVals) == 2 && (s1[incorrectVals[0]] == s2[incorrectVals[1]]) && (s2[incorrectVals[0]] == s1[incorrectVals[1]])
 }
 // @leet end
