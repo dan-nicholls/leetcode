f25854b3ffe51bfd6c0c5ec9ecae024d102581ef
Dan Nicholls <dannicholls12@gmail.com>
2026-03-02 08:54:58 +1100

    feat: add solution for #1455

---
+++ 1455.check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.go
@@ -0,0 +1,16 @@
+// @leet start
+import (
+	"strings"
+)
+
+func isPrefixOfWord(sentence string, searchWord string) int {
+	words := strings.Split(sentence, " ")
+	for i, word := range words {
+		if strings.HasPrefix(word, searchWord) {
+			return i + 1
+		}
+	}
+	return -1
+}
+
+// @leet end
